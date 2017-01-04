d9f70726ff13b4ef46a00f621094b7e208b2b7cb
DeMoss15 <mossur15@gmail.com>
2017-01-04 17:12:50 +0200

    first commit

---
+++ app/src/main/java/com/example/danielmossur/calculator/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.danielmossur.calculator;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button plus;
+    Button minus;
+    Button multiple;
+    Button division;
+    Button one;
+    Button two;
+    Button three;
+    Button four;
+    Button five;
+    Button six;
+    Button seven;
+    Button eight;
+    Button nine;
+    Button zero;
+    Button equal;
+    Button dot;
+    String outLine;
+
+    public void addSymb(char ch) {
+        if (Character.isDigit(textView.getText().charAt(textView.getText().length()-1))) {
+            textView.setText(textView.getText() + " " + ch + " ");
+        } /*else
+            textView.setText((StringBuilder)textView.getText().toString().setCharAt((textView.getText().length()-1), ch));*/
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        plus = (Button) findViewById(R.id.plus);
+        minus = (Button) findViewById(R.id.minus);
+        multiple = (Button) findViewById(R.id.multiple);
+        division = (Button) findViewById(R.id.division);
+        one = (Button) findViewById(R.id.one);
+        two = (Button) findViewById(R.id.two);
+        three = (Button) findViewById(R.id.three);
+        four = (Button) findViewById(R.id.four);
+        five = (Button) findViewById(R.id.five);
+        six = (Button) findViewById(R.id.six);
+        seven = (Button) findViewById(R.id.seven);
+        eight = (Button) findViewById(R.id.eight);
+        nine = (Button) findViewById(R.id.nine);
+        zero = (Button) findViewById(R.id.zero);
+        equal = (Button) findViewById(R.id.equal);
+        dot = (Button) findViewById(R.id.dot);
+        outLine = getString(R.string.out_line);
+
+        View.OnClickListener OnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == null || !(v instanceof TextView)) {
+                    return; //нет свойства text
+                }
+                switch (v.getId()) {
+                    case R.id.plus:
+                        addSymb('+');
+                        break;
+                    case R.id.minus:
+                        addSymb('-');
+                        break;
+                    case R.id.multiple:
+                        addSymb('*');
+                        break;
+                    case R.id.division:
+                        addSymb('/');
+                        break;
+                    case R.id.dot:
+                        textView.setText(textView.getText() + ".");
+                        break;
+                    case R.id.equal:
+                        addSymb('=');
+                        break;
+                    default:
+                        textView.setText(textView.getText() + ((TextView) v).getText().toString());
+                }
+            }
+        };
+
+
+        plus.setOnClickListener(OnClickListener);
+        minus.setOnClickListener(OnClickListener);
+        multiple.setOnClickListener(OnClickListener);
+        division.setOnClickListener(OnClickListener);
+        one.setOnClickListener(OnClickListener);
+        two.setOnClickListener(OnClickListener);
+        three.setOnClickListener(OnClickListener);
+        four.setOnClickListener(OnClickListener);
+        five.setOnClickListener(OnClickListener);
+        six.setOnClickListener(OnClickListener);
+        seven.setOnClickListener(OnClickListener);
+        eight.setOnClickListener(OnClickListener);
+        nine.setOnClickListener(OnClickListener);
+        zero.setOnClickListener(OnClickListener);
+        equal.setOnClickListener(OnClickListener);
+        dot.setOnClickListener(OnClickListener);
+    }
+
+    private void addSymb() {
+    }
+}
