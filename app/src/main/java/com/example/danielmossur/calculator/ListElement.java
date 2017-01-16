61b5bb68f74b67b83b20221a2624a61010ec8360
DeMoss15 <mossur15@gmail.com>
2017-01-16 15:10:37 +0200

    Toasts !
    Тосты! Всем тосты!)

---
+++ app/src/main/java/com/example/danielmossur/calculator/ListElement.java
@@ -0,0 +1,13 @@
+package com.example.danielmossur.calculator;
+
+/**
+ * Created by Daniel Mossur on 16.01.2017.
+ */
+
+class ListElement{
+    ListElement next;
+    String data;
+    char operator;
+    int fraction_range;
+    boolean fraction_path;
+}
